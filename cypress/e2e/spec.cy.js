e175dc1e6b114356f495df636a3b2a1f0fad53cb
Thiwanka Somachandra <thivankas@outlook.com>
2024-05-01 09:58:10 +0300

    11.9 Simple end-to-end tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,13 @@
-describe('Pokedex', function() {
-  it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+describe('Pokedex', () =>  {
+  it('front page can be opened', () => {
+    cy.visit('')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+  it('pokemon page can be navigate to', () => {
+    cy.visit('')
+    cy.contains('charmander').click()
+    cy.wait(5000)
+    cy.contains('solar power')
   })
 })
